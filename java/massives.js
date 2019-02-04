df0727a808004b732c363d6ab89c4d55bd60fefd
Shibuiul <www.AGU@mail.ru>
2019-02-04 17:37:25 +0300

    smh

--- java/massives.js
+++ java/massives.js
@@ -1,5 +1,14 @@
 function getRandomArbitrary(min, max) {
-  var randomMassive = [len];
-  len = Math.random() * (max - min) + min;
+  var randomMassive = {};
+  randomMassive.lenght = Math.random() * (max - min) + min;
   return randomMassive;
+}
+
+
+function massiveOfRandomNum(len) {
+	var randomMassive = {};
+	randomMassive.lenght = len;
+	for (var i = 0; randomMassive[i] < randomMassive.length; i++) {
+		randomMassive[i] = Math.random();
+	}
 }
