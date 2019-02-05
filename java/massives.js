f6df63fb994e57603b8d38cdb3faaa44f92aa5d3
Shibuiul <www.AGU@mail.ru>
2019-02-05 09:40:49 +0300

    4 done

--- java/massives.js
+++ java/massives.js
@@ -1,5 +1,5 @@
 function getRandomArbitrary(min, max) {
-  var randomMassive = {};
+  var randomMassive = [];
   randomMassive.lenght = Math.random() * (max - min) + min;
   return randomMassive;
 }
@@ -13,3 +13,65 @@ function getRandomArray(len) {
 	}
 	return randomArray;
 }
+
+//2 
+
+function getRandomArray(len) {
+	var randomArray = [];
+	randomArray.lenght = len;
+	for (var i = 0; i < len; i++) {
+		randomArray[i] = Math.random();
+	}
+	return randomArray;
+	var test = 0
+	for (var i = 0; i < len; i++) {
+		test += randomArray[i];
+	}
+	var test = test / len;
+	if (randomArray[i] > test) {
+		console.log(randomArray[i]);
+	}
+}
+
+//3
+
+function compareNumeric(a, b) {
+  return a - b;
+}
+
+function getRandomArray(len) {
+	var randomArray = [];
+	randomArray.lenght = len;
+	for (var i = 0; i < len; i++) {
+		randomArray[i] = Math.random();
+	}
+	randomArray.sort(compareNumeric);
+	return randomArray[0, 1];
+}
+
+//4
+
+function getRandomArray(len) {
+	var randomArray = [];
+	randomArray.lenght = len;
+	for (var i = 0; i < len; i++) {
+		randomArray[i] = Math.random();
+	}
+	if (randomArray[i] < 0.3) {
+		randomArray[i] = 0;
+	}
+}/*4.1*/
+
+function getRandomArray(len) {
+	var randomArray = [];
+	randomArray.lenght = len;
+	for (var i = 0; i < len; i++) {
+		randomArray[i] = Math.random();
+	}
+	if (randomArray[i] < 0.3) {
+		randomArray.splice(i, 1, "0");
+	}
+}/*4.2*/
+
+//5
+
