ea8f2388d6b0c623c86bd833cf529a1a5566009d
Shibuiul <www.AGU@mail.ru>
2019-02-10 11:17:05 +0300

    ~1

---
+++ java/inbuilt.js
@@ -0,0 +1,20 @@
+function getRandomArbitrary(min, max, len) {
+  var randomArr = [];
+  randomArr.lenght = len;
+  for (var i = 0; i < len; i++) {
+	randomArr[i] = Math.random() * (max - min) + min;
+}
+  return randomArr;
+}
+
+function rebuildArr(getRandomArbitrary) {
+	var arrayObject = {};
+	for (var i = 0; i < len; i++) {
+		arrayObject{ 
+			initial: randomArr[i],
+			sqrt: Math.sqrt(randomArr[i]),
+			floor: if (randomArr[i].toFixed(1))
+		}
+	}
+	return arrayObject;
+}
